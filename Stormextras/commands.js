99be2b1234b7ea64d338e3623b9026b063a6bfd6
C0nn0r167 <105995250+C0nn0r167@users.noreply.github.com>
2023-08-12 15:20:33 +0100

    Delete commands.js

--- Stormextras/commands.js
+++
@@ -1,4 +0,0 @@
-//DUNGEONS
-register("command", (floor) => {
-    if(floor == "m7"){ChatLib.chat("Joining mastermode floor 7")}if(floor == "mastermode"){ChatLib.chat("&eJoining mastermode floor 7")}if(floor == "f7"){ChatLib.chat("Joining floor 7")}if(floor == "normal"){ChatLib.chat("&eJoining floor 7")}
-}).setName("jd");
