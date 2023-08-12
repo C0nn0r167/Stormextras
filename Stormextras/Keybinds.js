2e090fdd8e2f872788ed464e9f586d99f16e93d4
C0nn0r167 <105995250+C0nn0r167@users.noreply.github.com>
2023-08-12 15:22:43 +0100

    Update Keybinds.js

--- Stormextras/Keybinds.js
+++ Stormextras/Keybinds.js
@@ -16,6 +16,17 @@ new KeyBind("Bazzar", Keyboard.LEFT, "!§4Stormalpha").registerKeyPress(() => {
     ChatLib.command("bz)
 })
 
+let sa = false
+
+register("renderOverlay", myRenderOverlay);
+
+function myRenderOverlay() {
+    if(sa == false){
+        Renderer.drawString("&e&lRight_Control for gui", 435, 300);
+        Renderer.drawString("&e(Will disapear when the gui is opened)", 395, 310)
+    }
+}
+
 new KeyBind("StormGUI", Keyboard.KEY_RCONTROL, "!§4Stormalpha").registerKeyPress(() => {
     Settings.openGUI()
     sa = true
